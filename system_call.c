be19636094e4a96ab5db0c56c8453902631a3448
JayceH <jayceh@mail.usf.edu>
2019-01-31 09:32:24 -0500

    Fixed system_call.c measuring length of open, read, and close to make it only measure length of read

--- system_call.c
+++ system_call.c
@@ -1,28 +1,35 @@
-//Program to measure length of system calls
+//Program to measure length of system calls (read)
 
 #include <stdio.h>
 #include <sys/time.h>
 #include <fcntl.h>
 int main() {
-
+	//Set the number of times you want to test read length
 	int sampleSize = 1000;
 	int i;
 	double totalTimeMicro;
 	for(i = 0; i < sampleSize; i++)
 	{
 		struct timeval start, end;
-
+		
+		int openFile = open("rfile.txt", O_RDONLY);
+		
+		//gettimeofday called immediatly before and
+		//immediatly after read sys call is called
+		//to measure time.
 		gettimeofday(&start, NULL);
 
-		int openFile = open("rfile.txt", O_RDONLY);
 		read(openFile, NULL, 1);
-		close(openFile);
 	
 		gettimeofday(&end, NULL);
-	
+
+		close(openFile);
+		
+		//.tv_usec used to extract the time in microseconds
 		int endtime = end.tv_usec;
 		int starttime = start.tv_usec;
 		int ellapsedtime = endtime - starttime;
+		//Adding to running total
 		totalTimeMicro += ellapsedtime;
 	}
 	double totalTimeMilli = totalTimeMicro/1000;
